4dc94c973bd9c4266109e00c29d4b228dfaf9993
orchsik <orchsik@naver.com>
2022-04-03 07:41:20 +0900

    add: cascade syntax

--- lib/fp/cascade.dart
+++ lib/fp/cascade.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 void testFp() {
   List<int> even = [2, 4, 6, 8];
   List<int> odd = [1, 3, 5, 7];
@@ -5,4 +7,21 @@ void testFp() {
   List<int> all = [...odd, ...even];
   all.sort();
   print(all);
+}
+
+void testCascade() {
+  var button = querySelector('#confim');
+  button?.text = 'Confirm';
+  button?.classes.add('important');
+  button?.onClick.listen((event) => window.alert('Confrimed!'));
+
+// Although it still invokes someMethod() on myObject, the result of the
+// expression isn’t the return value — it’s a reference to myObject!
+// ```dart
+// myObject..someMethod()
+// ```
+  querySelector('#confirm')
+    ?..text = 'Confirm'
+    ..classes.add('important')
+    ..onClick.listen((event) => window.alert('confirmied'));
 }
